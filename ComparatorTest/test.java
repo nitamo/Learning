3cda9c5e3aa0ff8d86a6878c7a5980e99d997098
Sergey Solomatin <nitamo@yandex.ru>
2016-10-24 23:30:13 +0300

    Back and direct sorting test

--- ComparatorTest/test.java
+++ ComparatorTest/test.java
@@ -11,16 +11,32 @@ class test {
 		System.out.println("Before sorting");
 		printBooks(booksList);
 		Collections.sort(booksList);
-		System.out.println("After sorting using Collections.sort");
+		System.out.println("\nAfter sorting using Collections.sort");
 		printBooks(booksList);
 		Collections.sort(booksList,
 			Comparator.comparingInt(Book::getId));
-		System.out.println("After sorting using nitty Comparator");
+		System.out.println("\nAfter sorting using nitty Comparator");
 		printBooks(booksList);
+
+		System.out.println("\nTesting sorting order: direct, backwards");
+		reverseComparatorTest(true);
+		reverseComparatorTest(false);
 	}
 
 	public static void printBooks(List<? extends Book> booksList) {
 		for(Book b : booksList)
 			System.out.println(b);
+	}
+
+	public static void reverseComparatorTest(boolean direct) {
+		Comparator<Integer> direComp = (o1, o2) -> o1 - o2;
+		Comparator<Integer> backComp = (o1, o2) -> o2 - o1;
+
+		List<Integer> list = Arrays.asList(5, 4, 7, 1);
+
+		Comparator<Integer> comp = direct ? direComp : backComp;
+
+		Collections.sort(list, comp);
+		System.out.println(Arrays.toString(list.toArray()));
 	}
 }
