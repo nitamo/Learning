e7a6f6448cb7274211af97f4d8616dc141b1d83b
Sergey Solomatin <nitamo@yandex.ru>
2016-10-24 17:42:59 +0300

    More Generics

--- GenericsTest/1/test.java
+++ GenericsTest/1/test.java
@@ -1,11 +1,12 @@
 import java.util.*;
 class test {
 	public static void main(String... args) {
-		List<? extends Number> ls1; 
-		//List<?> ls1;
 		
 		List<Integer> lsi1 = new ArrayList<>();
 		lsi1.add(new Integer(1));
+		
+		//		Upper-bounds
+		List<? extends Number> ls1; 
 
 		//We can remove objects from an upper-bounded List
 		//But we cannot add objects
@@ -17,5 +18,17 @@ class test {
 		System.out.println(lsi1);
 		System.out.println(ls1);
 
+		//		Unbounded
+		List<?> ls2;
+		ls2 = lsi1;
+		System.out.println(ls2);
+		//ls2.add(new Integer(1)); //this will not compile (Integer cannot be converted to #CAP1 which extends Number)
+
+		//		Lower-bounds
+		List<? super Integer> ls3;
+		ls3 = lsi1;
+		ls3.add(new Integer(3));
+		//ls3.add(new Object()); //this will not compile (Object cannot be converted to #CAP1 which extends Object and supers Integer)
+		System.out.println(ls3);
 	}
 }
