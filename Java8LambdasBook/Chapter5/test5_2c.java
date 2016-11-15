267df1be4bd3535f56e4179d458688e1f19e0c7f
Sergey Solomatin <ssolomatin@artec-group.com>
2016-11-15 17:28:44 +0300

    Java 8 Lambdas Book Chapter 5 groupingBy answer added

--- Java8LambdasBook/Chapter5/test5_2c.java
+++ Java8LambdasBook/Chapter5/test5_2c.java
@@ -1,21 +1,75 @@
-import java.util.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toList;
+import java.util.function.*;
+import java.util.*;
 
-class test5_2c {
-	public static void main(String... args) {
-		Stream<String> names = Stream.of("John", "Paul", "George", "John",
-				"Paul", "John", "Ringo", "Ringo", "Ringo"); 
-		
-		Map<Long, List<String>> out = names.collect(collectingAndThen(groupingBy(name -> name, Collectors.counting()), test5_2c::transformMap));
-		System.out.println( out);
+class GroupingByCollector<T, K>  implements Collector<T, Map<K, List<T>>, Map<K, List<T>>> {
+
+	Function<? super T, ? extends K> classifier;
+	
+	public GroupingByCollector(Function<? super T, ? extends K> classifier) {
+		this.classifier = classifier;
 	}
 
-	public static Map<Long, List<String>> transformMap(Map<String, Long> input) {
-		return input.entrySet().stream()
-					.collect(groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, toList())));
+	@Override
+	public Supplier<Map<K, List<T>>> supplier() {
+		return () -> new HashMap<K, List<T>>();
+	}
+
+	@Override
+	public BiConsumer<Map<K, List<T>>, T> accumulator() {
+		return (acc, it) -> {
+				K key = classifier.apply(it);
+				consume(acc, key, it);
+			};
+	}
+
+	@Override
+	public BinaryOperator<Map<K, List<T>>> combiner() {
+		return (first, second) -> {
+			Map<K, List<T>> resultMap = new HashMap<>();
+
+			first.keySet().stream()
+				.forEach(key -> resultMap.merge(key, first.get(key), this::remapLists) );
+			second.keySet().stream()
+				.forEach(key -> resultMap.merge(key, second.get(key), this::remapLists) );
+			return resultMap;
+		};
+	}
+
+	@Override
+	public Function<Map<K, List<T>>, Map<K, List<T>>> finisher() {
+		return (acc) -> acc;
+	}
+
+	@Override
+	public Set<Collector.Characteristics> characteristics() {
+		return EnumSet.of(Collector.Characteristics.UNORDERED);
+	}
+						
+	
+	public void consume(Map<K, List<T>> acc, K key, T it) {
+		if(acc.containsKey(key))
+			acc.get(key).add(it);
+		else {
+			List<T> list = new ArrayList<>();
+			list.add(it);
+			acc.put(key, list);
+		}
+	}
+
+	public List<T> remapLists(List<T> first, List<T> second) {
+		first.addAll(second);
+		return first;
+	}
+}
+
+public class test5_2c {
+	public static void main(String... args) { 
+		List<String> list = Arrays.asList("First", "Second", "Third", "Fifth", "Sixth", "Seventh");
+
+		Map<Integer, List<String>> map = list.stream()
+			.collect(new GroupingByCollector<String, Integer>( it -> it.length() ) );
+
+		System.out.println( map );
 	}
 }
